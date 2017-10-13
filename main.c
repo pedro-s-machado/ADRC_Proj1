a3e70b7da630e545d3354c8faa7f138d5bcdd63d
pedro-s-machado <scucpsm@hotmail.com>
2017-10-13 23:46:02 +0100

    Main updated

--- main.c
+++ main.c
@@ -14,33 +14,76 @@
 int main(int argc, const char * argv[]) {
     
     printf(" ### ADRC 1° semestre 2017-1018 - Assignment 1 ### \n\n");
-    printf("To list all possible commands, type help and press enter, \n\n");
+    printf("To list all possible commands, type h and press enter, \n\n");
     
     char *line = NULL;
+    char *line2 = NULL;
     char **args = NULL;
     int status = 1;
     size_t bufsize = 0;
 
     oneBitNode *root = NULL;
     twoBitNode *root2 = NULL;
-    char testInsert[17] ="010";
-    char testPrefix[17] = "001";
     
     do {
-        printf(" >");
+        printf("\n >");
         bufsize = 0; // have getline allocate a buffer for us
         getline(&line, &bufsize, stdin);
-        if (line[0] == 'q') status = 0;
-        else if(line[0] == 'i') root = InsertPrefix(root, testInsert, 9);
-        else if(line[0] == 'n') root = PrefixTree("PrefixTable.txt");
-        else if(line[0] == 'p') printTree2(root);
-        else if(line[0] == 'o') printTree(root, 0, -1);
-        else if(line[0] == 'f') printf("Next Hop for %s is %d\n", testPrefix, LookUp1(root, testPrefix));
-        else if(line[0] == 'd') root = DeletePrefix(root, testInsert);
-        else if(line[0] == 't') PrintTable(root);
-        else if(line[0] == 'y') root2 = BinaryToTwoBit(root, -1);
-        else if(line[0] == 'z') print2BTree(root2, 0, "top");
-        else if(line[0] == 'x') PrintTableEven(root2);
+        
+        if (line[0] == 'q'){
+            status = 0;
+        }
+        else if(line[0] == 'i'){
+            printf("Insert a Prefix(16 bit): >");
+            bufsize = 0; // have getline allocate a buffer for us
+            getline(&line, &bufsize, stdin);
+
+            printf("Insert the NextHop(int): >");
+            bufsize = 0; // have getline allocate a buffer for us
+            getline(&line2, &bufsize, stdin);
+            root = InsertPrefix(root, line, atoi(line2));
+        } 
+        else if(line[0] == 'o'){
+            printf("Insert the name of the file: >");
+            bufsize = 0; // have getline allocate a buffer for us
+            scanf("%s", line);
+            root = PrefixTree(line);
+            getline(&line, &bufsize, stdin);
+        }
+        else if(line[0] == 'l'){
+            printf("Insert the prefix(16 bit): >");
+            scanf("%s", line);
+            printf("Next Hop for %s is %d\n", line, LookUp1(root, line));   
+            getline(&line, &bufsize, stdin);
+        }
+        else if(line[0] == 'd'){
+            printf("Insert the prefix(16 bit): >");
+            scanf("%s", line);
+            root = DeletePrefix(root, line);
+            getline(&line, &bufsize, stdin);
+        }
+        else if(line[0] == 'p'){
+            PrintTable(root);
+        }
+        else if(line[0] == 'b'){
+            root2 = BinaryToTwoBit(root, -1);
+        }
+        else if(line[0] == 'v'){
+            PrintTableEven(root2);
+        }else if(line[0] == 'h'){
+            printf("o - PrefixTree\n");
+            printf("p - PrintTable\n");
+            printf("l - LookUp\n");
+            printf("i - InsertPrefix\n");
+            printf("d - DeletePrefix\n");
+            printf("b - BinaryToTwoBit\n");
+            printf("v - PrintTableEven\n");
+            printf("h - Help\n");
+            printf("q - Quit\n");
+        }
+        else{
+            printf("Not a known command\n");
+        }
     } while (status);
     
     free(line);
